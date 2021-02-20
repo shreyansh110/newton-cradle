0079e1cfc5fcc58f46f32ce4c01b7b964a7139fc
shreyansh110 <69755759+shreyansh110@users.noreply.github.com>
2021-02-20 19:41:48 +0530

    Add files via upload

--- newtonscradle/MatterJSBoilerPlate-master/sketch.js
+++ newtonscradle/MatterJSBoilerPlate-master/sketch.js
@@ -2,9 +2,9 @@ const Engine=Matter.Engine
 const World=Matter.World
 const Bodies=Matter.Bodies
 const Body=Matter.Body
-//const Constraint=Matter.Constraint;
+const Constraint=Matter.Constraint;
 
-var handle,bob1;
+var handle,bob1,bob2,bob3,bob4,bob5,BallDiameter;
 
 function preload(){
 
@@ -16,11 +16,14 @@ createCanvas(1000,600)
   engine=Engine.create();
   world=engine.world;
 
-handle=new Support(450,130,270,10);
- bob1=new Ball(550,170,10);
+ handle=new Support(460,130,220,10);
+ bob1=new Ball(550,350,21);
+ bob2=new Ball(508,350,21);
+ bob3=new Ball(466,350,21);
+ bob4=new Ball(424,350,21);
+ bob5=new Ball(382,350,21);
 
-
-
+string=new Chain(handle.body,bob1.body,BallDiameter*2,0)
 }
 
 
@@ -30,6 +33,11 @@ rectMode(CENTER)
 background(225);
 handle.display();
 bob1.display();
+bob2.display();
+bob3.display();
+bob4.display();
+bob5.display();
+string.display();
 drawSprites();
 
 
